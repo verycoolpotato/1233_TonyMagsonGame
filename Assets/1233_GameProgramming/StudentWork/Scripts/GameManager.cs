c36c45b6e718a14c94ab6ca4112f895a1550608d
verycoolpotato <arandmagson@gmail.com>
2025-05-21 14:27:37 -0700

    Properly Set up multiple scenes - character, GameRunscene, world

--- Assets/1233_GameProgramming/StudentWork/Scripts/GameManager.cs
+++ Assets/1233_GameProgramming/StudentWork/Scripts/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
     }
     private void InitializeGame()
     {
-        levelManager.LoadLevelAdditively("SimpleLevel");
+        levelManager.LoadLevelAdditively("World");
         characterManager.SpawnCharacter();
     }
 
