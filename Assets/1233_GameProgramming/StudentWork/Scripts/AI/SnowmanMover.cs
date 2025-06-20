d36f687a177392d79c2b6fab0a1646c1965af14e
verycoolpotato <arandmagson@gmail.com>
2025-06-20 13:13:09 -0700

    Basic Version of Enemy AI, only has random actions

--- Assets/1233_GameProgramming/StudentWork/Scripts/AI/SnowmanMover.cs
+++ Assets/1233_GameProgramming/StudentWork/Scripts/AI/SnowmanMover.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class SnowmanMover : MonoBehaviour
 {
-    private Vector3 targetPos;
+   
     private enum AIStates
     {
         Charge,
@@ -16,73 +16,88 @@ public class SnowmanMover : MonoBehaviour
     [SerializeField] private float rotateSpeed;
     [SerializeField] private float chargeSpeed;
     [SerializeField] private float strafeSpeed;
-    [SerializeField] private AIStates state; 
 
+    [Tooltip("Current state of the enemy AI")]
+    [SerializeField] private AIStates state;
+
+    [Tooltip("How far the player is knocked back on contact with this character")]
     [SerializeField] public float knockback;
     
     [SerializeField] private NavMeshAgent _agent;
 
+    [Tooltip("Multiplies the duration of each state (Default is 10)")]
+    [SerializeField] private float stateDurationMultiplier = 10;
+
+    //How long the state lasts
+    private float stateDuration = 0.4f;
+
+    private Vector3 targetPos;
+
     private void Start()
     {
+        InvokeRepeating(nameof(ShuffleState), 1,stateDuration * stateDurationMultiplier);
         _agent.updateRotation = false;
     }
     private void Update()
     {
-        stateSwitcher();
-
-
+        StateSwitcher();
         targetPos = PlayerLocatorSingleton.Instance.transform.position;
-
         FacePlayer();
     }
+
+    private void ShuffleState()
+    {
+      int newState = Random.Range(0, 3);
+        state = (AIStates)newState;
+    }
+
     private void FacePlayer()
     {
         Vector3 LookPos = targetPos - transform.position;
-
         LookPos.y = 0;
-
        Quaternion rotation = Quaternion.LookRotation(LookPos);
-
         transform.rotation = Quaternion.Slerp(transform.rotation,rotation,Time.deltaTime * rotateSpeed);
     }
-    private void stateSwitcher()
+
+    private void StateSwitcher()
     {
         if (PlayerLocatorSingleton.Instance != null && _agent.enabled)
         {
             switch (state)
             {
                 case AIStates.Charge:
-                    chargeState();
+                    ChargeState();
                     break;
 
                 case AIStates.StrafeLeft:
-                    strafeState(1);
+                    StrafeState(1);
                     break;
 
                 case AIStates.StrafeRight:
-                    strafeState(-1);
+                    StrafeState(-1);
                     break;
             }
         }
-        
     }
-    private void chargeState()
+
+    private void ChargeState()
     {
-        
             _agent.destination = targetPos;
             _agent.speed = chargeSpeed;
-            
-        
-        
+
+
+        stateDuration = 0.5f;
     }
-    private void strafeState(float strafeDir)
+
+    private void StrafeState(float strafeDir)
     {
-        
         var dir = Vector3.Cross(targetPos - transform.position, Vector3.up);
 
         _agent.SetDestination(transform.position + dir * strafeDir);
         _agent.speed = strafeSpeed;
-        
+
+        stateDuration = 0.08f;
+
     }
    
    
